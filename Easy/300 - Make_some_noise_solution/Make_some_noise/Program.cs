fb0e62f0adf4f9c425b881a6087a7b672e68d786
I321661 <s.staev1@gmail.com>
2017-02-16 04:50:14 +0100

    Changed the folders format

---
+++ Easy/300 - Make_some_noise_solution/Make_some_noise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Make_some_noise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int note = 260;
+            for( int i = 0 ; i < 8 ; i ++)
+            {
+                Console.Beep(note, 400);
+                note += 70;
+            }
+        }
+    }
+}
